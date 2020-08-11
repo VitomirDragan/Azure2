c97784655dc7aa9f860274ab7c7bf661800d0a91
VitomirDragan <vitomir_dragan86@yahoo.com>
2020-08-11 22:35:54 +0300

    should work

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
+import webbrowser
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, render_template, request, jsonify
 from firebase import firebase
-from time import sleep
-
-firebase = firebase.FirebaseApplication('https://temperaturemanagement-iot.firebaseio.com')
+import time
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST','GET'])
+firebase = firebase.FirebaseApplication('https://temperaturemanagement-iot.firebaseio.com')
+
+
+@app.route('/_room1', methods=['GET'])
 def room1():
+    result1 = firebase.get('CurrentTempRoom1', 'Value')
+    return jsonify(result=result1)
+
+
+@app.route('/_room2', methods=['GET'])
+def room2():
+    result = firebase.get('CurrentTempRoom2', 'Value')
+    return jsonify(result2=result)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def setRoom1():
     result = firebase.get('CurrentTempRoom1', 'Value')
     result2 = firebase.get('CurrentTempRoom2', 'Value')
     if request.method == 'POST':
@@ -17,8 +34,9 @@ def room1():
     else:
         return render_template('index.html', data1=result, data2=result2)
 
-@app.route('/room2', methods=['POST','GET'])
-def room2():
+
+@app.route('/setRoom2', methods=['POST', 'GET'])
+def setRoom2():
     result = firebase.get('CurrentTempRoom1', 'Value')
     result2 = firebase.get('CurrentTempRoom2', 'Value')
     if request.method == 'POST':
@@ -28,20 +46,11 @@ def room2():
     else:
         return render_template('index.html', data1=result, data2=result2)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-
-
-
-# firebase.put('DesiredTempRoom1', 'Value', 1)
-#
-# firebase.put('DesiredTempRoom2', 'Value', 30)
-#
-# while True:
-#     result = firebase.get('CurrentTempRoom1', 'Value')
-#     print("Temperature Room1:",result)
-#     result = firebase.get('CurrentTempRoom2', 'Value')
-#     print("Temperature Room2:", result)
-#     sleep(5)
+if __name__ == '__main__':
+    app.run()
